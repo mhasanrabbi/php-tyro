b0a1cb1a9a2f62588e698eebd768f7682cf83eeb
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-13 20:37:02 +0600

    get_defined_vars

---
+++ task-1/get_defined_vars.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * (PHP 4 >= 4.0.4, PHP 5, PHP 7, PHP 8)
+ * get_defined_vars — Returns an array of all defined variables
+ */
+
+$b = array(1, 1, 2, 3, 5, 8);
+
+$arr = get_defined_vars();
