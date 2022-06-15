375af360c3fae38856e93f80d4a1759fe3a4acae
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-15 13:45:10 +0600

    is_numeric

---
+++ task-1/is_numeric.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Finds whether a variable is a number or a numeric string 
+ */
+
+$tests = array(
+	"42",
+	1337,
+	0x539,
+	02471,
+	0b10100111001,
+	1337e0,
+	"0x539",
+	"02471",
+	"0b10100111001",
+	"1337e0",
+	"not numeric",
+	array(),
+	9.1,
+	null,
+	'',
+);
+
+foreach ($tests as $element) {
+	if (is_numeric($element)) {
+		echo var_export($element, true) . " is numeric", PHP_EOL;
+	} else {
+		echo var_export($element, true) . " is NOT numeric", PHP_EOL;
+	}
+}
