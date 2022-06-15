917af8261320c0af60e5ccc1112a6888408b83ae
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-15 13:20:59 +0600

    is_integer

--- task-1/is_int.php
+++ task-1/is_int.php
@@ -1,13 +1,24 @@
-<?php 
+<?php
+
 /**
-* (PHP 4, PHP 5, PHP 7, PHP 8)
-* is_int — Find whether the type of a variable is integer 
-*/
+ * (PHP 4, PHP 5, PHP 7, PHP 8)
+ * is_int — Find whether the type of a variable is integer 
+ */
 
 $values = array(23, "23", 23.5, "23.5", null, true, false);
 foreach ($values as $value) {
-    echo "is_int(";
-    var_export($value);
-    echo ") = ";
-    var_dump(is_int($value));
+	echo "is_int(";
+	var_export($value);
+	echo ") = ";
+	var_dump(is_int($value));
 }
+
+/**
+ * is_int(23) = bool(true)
+ * is_int('23') = bool(false)
+ * is_int(23.5) = bool(false)
+ * is_int('23.5') = bool(false)
+ * is_int(NULL) = bool(false)
+ * is_int(true) = bool(false)
+ * is_int(false) = bool(false)
+ */
---
+++ task-1/is_integer.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * The is_integer() function is used to test whether the type of the specified variable is an integer or not.
+ */
+
+$var_name1 = 678;
+$var_name2 = "a678";
+
+if (is_integer($var_name1)) {
+	echo "$var_name1 is Integer" . "\n";
+} else {
+	echo "$var_name1 is not an Integer" . "\n";
+}
+if (is_integer($var_name2)) {
+	echo "$var_name2 is Integer" . "\n";
+} else {
+	echo "$var_name2 is not Integer" . "\n";
+}
