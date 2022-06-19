1740f121b2d66a4bc3580d5e4c9c70fa84632d5b
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-19 19:54:01 +0600

    acosh.php

---
+++ task-3/math_functions/acosh.php
@@ -0,0 +1,6 @@
+<?php
+//Returns the inverse hyperbolic cosine of num, i.e. the value whose hyperbolic cosine is num.
+
+$x = 1;
+$y1 = acosh($x);
+echo "PHP function acosh(). acosh(" . $x . ") = " . $y1 . ".<br/>";
