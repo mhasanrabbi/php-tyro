c6b15cbf3bdfa8ef25ec01b67b507348f11c1e98
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-19 19:59:02 +0600

    asin.php

---
+++ task-3/math_functions/asin.php
@@ -0,0 +1,10 @@
+<?php
+
+// Returns the arc sine of num in radians. asin() is the inverse function of sin(), which means that a==sin(asin(a)) for every value of a that is within asin()'s range. 
+
+echo (asin(0.64) . "<br>"); //0.69449826562656
+echo (asin(-0.4) . "<br>"); //-0.41151684606749
+echo (asin(0) . "<br>"); //0
+echo (asin(-1) . "<br>"); //-1.5707963267949
+echo (asin(1) . "<br>"); //1.5707963267949
+echo (asin(2)); //NAN
