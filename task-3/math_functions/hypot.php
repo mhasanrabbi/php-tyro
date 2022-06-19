42f855be7d806ab1cee7139441d305d5d3a2b82b
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-19 20:50:59 +0600

    hypot

---
+++ task-3/math_functions/hypot.php
@@ -0,0 +1,8 @@
+<?php
+
+// hypot() returns the length of the hypotenuse of a right-angle triangle with sides of length x and y, or the distance of the point (x, y) from the origin. This is equivalent to sqrt(x*x + y*y). 
+
+echo hypot(3, 4) . "<br>";
+echo hypot(4, 6) . "<br>";
+echo hypot(1, 3) . "<br>";
+echo sqrt(3 * 3 + 4 * 4);
