1a9e3e07c7c23a4ff5cf83091914a5a7a2922765
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-19 20:05:52 +0600

    atan

---
+++ task-3/math_functions/atan.php
@@ -0,0 +1,9 @@
+<?php 
+//Returns the arc tangent of num in radians. atan() is the inverse function of tan(), which means that a==tan(atan(a)) for every value of a that is within atan()'s range. 
+
+echo(atan(0.50) . "<br>");
+echo(atan(-0.50) . "<br>");
+echo(atan(5) . "<br>");
+echo(atan(-5) . "<br>");
+echo(atan(100) . "<br>");
+echo(atan(-100));
