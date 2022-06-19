7e4c876a7488c563db9d3ea5c5b2953c8f2bdbe6
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-19 20:07:49 +0600

    atanh

---
+++ task-3/math_functions/atanh.php
@@ -0,0 +1,8 @@
+<?php 
+// Returns the inverse hyperbolic tangent of num, i.e. the value whose hyperbolic tangent is num. 
+
+echo(atanh(M_PI_4) . "<br>");
+echo(atanh(0.50) . "<br>");
+echo(atanh(-0.50) . "<br>");
+echo(atanh(1) . "<br>");
+echo(atanh(-1));
