6950273da39caf918f9d074ae1e8ad69b56e6ba1
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-19 21:18:08 +0600

    pi, pow

---
+++ task-3/math_functions/pi.php
@@ -0,0 +1,5 @@
+<?php
+
+// Get value of pi 
+echo pi(); // 3.1415926535898
+echo M_PI; // 3.1415926535898
---
+++ task-3/math_functions/pow.php
@@ -0,0 +1,9 @@
+<?php
+
+// Exponential expression
+var_dump(pow(2, 8)); // int(256)
+echo pow(-1, 20); // 1
+echo pow(0, 0); // 1
+echo pow(10, -1); // 0.1
+
+echo pow(-1, 5.5); // NAN
