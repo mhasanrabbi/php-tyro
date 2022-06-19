c383308090773dfdbc5cc7154b3a2dd4db88e11e
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-19 20:26:04 +0600

    cos & cosh

---
+++ task-3/math_functions/cos.php
@@ -0,0 +1,4 @@
+<?php 
+// cos() returns the cosine of the num parameter. The num parameter is in radians. 
+
+echo cos(M_PI); // -1
---
+++ task-3/math_functions/cosh.php
@@ -0,0 +1,8 @@
+<?php 
+// Returns the hyperbolic cosine of num, defined as (exp(arg) + exp(-arg))/2.
+
+echo(cosh(3) . "<br>");
+echo(cosh(-3) . "<br>");
+echo(cosh(0) . "<br>");
+echo(cosh(M_PI) . "<br>");
+echo(cosh(2*M_PI));
