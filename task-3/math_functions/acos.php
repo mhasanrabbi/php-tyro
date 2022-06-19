eda339a01482fca37515be84245b3eb915b0615f
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-19 19:52:03 +0600

    acos.php

---
+++ task-3/math_functions/acos.php
@@ -0,0 +1,9 @@
+<?php
+//Returns the arc cosine of num in radians. acos() is the inverse function of cos(), which means that a==cos(acos(a)) for every value of a that is within acos()' range.
+
+echo (acos(0.64) . "<br>");
+echo (acos(-0.4) . "<br>");
+echo (acos(0) . "<br>");
+echo (acos(-1) . "<br>");
+echo (acos(1) . "<br>");
+echo (acos(2));
