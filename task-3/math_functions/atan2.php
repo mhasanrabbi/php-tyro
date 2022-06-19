4e24741dfe23f9dc19d85e53a459fb036a8f191b
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-19 20:02:26 +0600

    atan2.php

---
+++ task-3/math_functions/atan2.php
@@ -0,0 +1,9 @@
+<?php 
+//This function calculates the arc tangent of the two variables x and y. It is similar to calculating the arc tangent of y / x, except that the signs of both arguments are used to determine the quadrant of the result. 
+
+echo(atan2(0.50,0.50) . "<br>");
+echo(atan2(-0.50,-0.50) . "<br>");
+echo(atan2(5,5) . "<br>");
+echo(atan2(10,20) . "<br>");
+echo(atan2(-5,-5) . "<br>");
+echo(atan2(-10,10));
