4afe96492f7b190e17f545f97230212cb7b40fb1
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-20 13:11:51 +0600

    nl2br, number_format

---
+++ task-3/string_functions/nl2br.php
@@ -0,0 +1,6 @@
+<?php
+
+//Inserts HTML line breaks before all newlines in a string
+
+$string = "This\r\nis\n\ra\nstring\r";
+echo nl2br($string);
---
+++ task-3/string_functions/number_format.php
@@ -0,0 +1,7 @@
+<?php
+
+//Format a number with grouped thousands
+
+echo number_format("1000000") . "<br>";
+echo number_format("1000000", 2) . "<br>";
+echo number_format("1000000", 2, ",", ".");
