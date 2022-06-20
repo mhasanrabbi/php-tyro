68d93f523e51caf8db1f0a5cda39c039e885d023
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-20 11:48:55 +0600

    convert_uuencode, uudecode

---
+++ task-3/string_functions/convert_uudecode.php
@@ -0,0 +1,5 @@
+<?php
+
+// Decode a uuencoded string
+$str = ",2&5L;&\@=V]R;&0A `";
+echo convert_uudecode($str);
---
+++ task-3/string_functions/convert_uuencode.php
@@ -0,0 +1,6 @@
+<?php
+
+//Encode a string
+
+$str = "Hello world!";
+echo convert_uuencode($str);
