2ad05a3839851f580696260612c93cf37bcde804
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-20 11:29:26 +0600

    addcslashes, addslashes

---
+++ task-3/string_functions/addcslashes.php
@@ -0,0 +1,4 @@
+<?php
+
+//  Quote string with slashes in a C style
+echo addcslashes('foO[ ]', 'A..z');
---
+++ task-3/string_functions/addslashes.php
@@ -0,0 +1,8 @@
+<?php
+
+// Quote string with slashes
+
+$str = "Is your name O'Reilly?";
+
+// Outputs: Is your name O\'Reilly?
+echo addslashes($str);
