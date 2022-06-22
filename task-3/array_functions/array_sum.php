02f1062d328fa48c00e38c34afeaccca1236b3e7
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 13:38:01 +0600

    array_sum

---
+++ task-3/array_functions/array_sum.php
@@ -0,0 +1,9 @@
+<?php
+
+// Calculate the sum of values in an array
+
+$a = array(2, 4, 6, 8);
+echo "sum(a) = " . array_sum($a) . "\n";
+
+$b = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
+echo "sum(b) = " . array_sum($b) . "\n";
