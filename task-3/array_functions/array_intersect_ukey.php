32a97ce3442314ed4c731f0e50760050d01d885e
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 12:34:22 +0600

    array_intersect_key/_ukey

---
+++ task-3/array_functions/array_intersect_ukey.php
@@ -0,0 +1,8 @@
+<?php
+
+//Computes the intersection of arrays using a callback function on the keys for comparison
+
+$array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3, 'purple' => 4);
+$array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan'   => 8);
+
+var_dump(array_intersect_key($array1, $array2));
