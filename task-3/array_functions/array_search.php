271816e44a6a80c2e9478f944aa73f119f155e4a
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 13:23:05 +0600

    array_search

---
+++ task-3/array_functions/array_search.php
@@ -0,0 +1,5 @@
+<?php
+
+// Searches the array for a given value and returns the first corresponding key if successful
+$a = array("a" => "red", "b" => "green", "c" => "blue");
+echo array_search("red", $a);
