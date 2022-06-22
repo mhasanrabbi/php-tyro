4bc045953c3f6f9c7d20a033840f7f3d3767d3c5
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 12:22:37 +0600

    array_intersect/_assoc/_uassoc

---
+++ task-3/array_functions/array_intersect_assoc.php
@@ -0,0 +1,7 @@
+<?php
+//Compare the keys and values of two arrays, and return the matches
+
+$array1 = array("a" => "green", "b" => "brown", "c" => "blue", "red");
+$array2 = array("a" => "green", "b" => "yellow", "blue", "red");
+$result_array = array_intersect_assoc($array1, $array2);
+print_r($result_array);
