1e5ee8a0dc52054772386a3addb31a90f2bd339b
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 12:58:28 +0600

    array_merge/_merge_recursive

---
+++ task-3/array_functions/array_merge.php
@@ -0,0 +1,8 @@
+<?php
+
+// Merge one or more arrays
+
+$array1 = array("color" => "red", 2, 4);
+$array2 = array("a", "b", "color" => "green", "shape" => "trapezoid", 4);
+$result = array_merge($array1, $array2);
+print_r($result);
---
+++ task-3/array_functions/array_merge_recursive.php
@@ -0,0 +1,8 @@
+<?php
+
+// Merge one or more arrays recursively
+
+$ar1 = array("color" => array("favorite" => "red"), 5);
+$ar2 = array(10, "color" => array("favorite" => "green", "blue"));
+$result = array_merge_recursive($ar1, $ar2);
+print_r($result);
