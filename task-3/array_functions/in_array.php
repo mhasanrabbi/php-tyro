dae6fb1896e877bfa8b544dc6b33dd922016d7ae
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 14:02:26 +0600

    in_array

---
+++ task-3/array_functions/in_array.php
@@ -0,0 +1,10 @@
+<?php
+//Checks if a value exists in an array
+$os = array("Mac", "NT", "Irix", "Linux");
+if (in_array("Irix", $os)) {
+	echo "Got Irix";
+}
+if (in_array("mac", $os)) {
+	echo "Got mac";
+}
+?>
