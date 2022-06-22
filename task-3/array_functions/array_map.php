16f002fe241928697a1baad6b82b71d568fbc4b8
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 12:55:44 +0600

    array_map

---
+++ task-3/array_functions/array_map.php
@@ -0,0 +1,12 @@
+<?php
+
+// Applies the callback to the elements of the given arrays
+
+function cube($n)
+{
+	return ($n * $n * $n);
+}
+
+$a = [1, 2, 3, 4, 5];
+$b = array_map('cube', $a);
+print_r($b);
