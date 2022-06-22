b63e8cd1ec313c758da47aa0ea36804bda60ad0d
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 13:45:46 +0600

    array_uintersect/_assoc/_uassoc

---
+++ task-3/array_functions/array_uintersect_uassoc.php
@@ -0,0 +1,26 @@
+<?php
+//Compare the keys and values of two arrays (using two user-defined functions for comparison) and return the matches
+
+function myfunction_key($a,$b)
+{
+if ($a===$b)
+  {
+  return 0;
+  }
+  return ($a>$b)?1:-1;
+}
+
+function myfunction_value($a,$b)
+{
+if ($a===$b)
+  {
+  return 0;
+  }
+  return ($a>$b)?1:-1;
+}
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue");
+$a2=array("a"=>"red","b"=>"green","c"=>"green");
+
+$result=array_uintersect_uassoc($a1,$a2,"myfunction_key","myfunction_value");
+print_r($result);
