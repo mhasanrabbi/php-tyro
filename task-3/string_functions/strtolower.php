fd15edc1aa3909a69842f521e0b016d1a3028268
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 11:10:45 +0600

    strtolower strtoupper

---
+++ task-3/string_functions/strtolower.php
@@ -0,0 +1,7 @@
+<?php
+
+// Make a string lowercase 
+
+$str = "Mary Had A Little Lamb and She LOVED It So";
+$str = strtolower($str);
+echo $str; // Prints mary had a little lamb and she loved it so
---
+++ task-3/string_functions/strtoupper.php
@@ -0,0 +1,5 @@
+<?php
+// Make a string uppercase 
+$str = "Mary Had A Little Lamb and She LOVED It So";
+$str = strtoupper($str);
+echo $str; // Prints MARY HAD A LITTLE LAMB AND SHE LOVED IT SO
