ebbb81ce433adae20725b791afda49a35de58f1b
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 13:14:16 +0600

    array_rand/_reduce

---
+++ task-3/array_functions/array_rand.php
@@ -0,0 +1,7 @@
+<?php
+
+//Pick one or more random keys out of an array
+$input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
+$rand_keys = array_rand($input, 2);
+echo $input[$rand_keys[0]] . "\n";
+echo $input[$rand_keys[1]] . "\n";
