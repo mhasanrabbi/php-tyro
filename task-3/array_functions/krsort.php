7a3095a68a9ad153b7d49b1f3bb3d6902b746537
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 14:07:07 +0600

    krsort ksort

---
+++ task-3/array_functions/krsort.php
@@ -0,0 +1,4 @@
+<?php
+//Sort an associative array in descending order, according to the key
+$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+krsort($age);
---
+++ task-3/array_functions/ksort.php
@@ -0,0 +1,4 @@
+<?php
+//Sort an associative array in ascending order, according to the key
+$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+ksort($age);
