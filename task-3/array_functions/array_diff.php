a0f5a2077a65fb6530e2aa69f733e40fb9eb6a22
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-06-22 12:04:36 +0600

    array_diff/_key/_ukey

---
+++ task-3/array_functions/array_diff.php
@@ -0,0 +1,9 @@
+<?php
+
+// Computes the difference of arrays
+
+$array1 = array("a" => "green", "red", "blue", "red");
+$array2 = array("b" => "green", "yellow", "red");
+$result = array_diff($array1, $array2);
+
+print_r($result);
